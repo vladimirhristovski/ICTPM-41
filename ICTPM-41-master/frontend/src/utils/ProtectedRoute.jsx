889cbdc70afc4963ad1156c6226b2fd0a6f6085f
Marinst9 <marinatrajko07@gmail.com>
2026-04-04 23:41:08 +0200

    fix: move Field CRUD files to correct location, remove nested folder

--- ICTPM-41-master/frontend/src/utils/ProtectedRoute.jsx
+++
@@ -1,9 +0,0 @@
-import {Navigate} from 'react-router-dom';
-import authService from '../services/authService';
-
-export default function ProtectedRoute({children}) {
-    if (!authService.isLoggedIn()) {
-        return <Navigate to="/login" replace/>;
-    }
-    return children;
-}
